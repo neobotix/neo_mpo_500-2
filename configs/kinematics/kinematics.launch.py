e8c6ff7866c252d62ed3be553359ce11aea35fc8
Pradheep <padhupradheep@gmail.com>
2021-05-12 14:17:26 +0000

    Bug-Fix: Launchdescription for mecanum kinematics accepted

--- configs/kinematics/kinematics.launch.py
+++ configs/kinematics/kinematics.launch.py
@@ -8,6 +8,6 @@ import launch_ros.actions
 def generate_launch_description():
     config = os.path.join(get_package_share_directory('neo_mpo_500-2'),'configs/kinematics','kinematics.yaml')
     return launch.LaunchDescription([
-        launch_ros.actions.Node(package='neo_kinematics_differential', executable='neo_differential_node', output='screen',
-            name='neo_differential_node', parameters = [config])
+        launch_ros.actions.Node(package='neo_kinematics_mecanum', executable='neo_mecanum_node', output='screen',
+            name='neo_mecanum_node', parameters = [config])
     ])
