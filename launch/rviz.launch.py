c8323ed88bf7585b00193863ca477d959fc1faa8
Adarsh Karan Kesavadas Prasanth <adarshkaran01@gmail.com>
2025-05-15 17:57:58 +0530

    fixed issue with rviz config default value

--- launch/rviz.launch.py
+++ launch/rviz.launch.py
@@ -106,7 +106,7 @@ def generate_launch_description():
             choices=['sick_s300', 'sick_microscan3'],
             description='Type of laser scanner to use\n\t'
         )
-    
+
     declare_gripper_type_cmd = DeclareLaunchArgument(
             'gripper_type', default_value='',
             choices=['', '2f_140', '2f_85', 'epick'],
@@ -121,8 +121,8 @@ def generate_launch_description():
     declare_rviz_cfg_arg = DeclareLaunchArgument(
             'rviz_config',
             default_value=os.path.join(
-            get_package_share_directory('mp_rviz'),
-            'rviz', 'robot_description_rviz.rviz'),
+            get_package_share_directory('neo_mpo_500-2'),
+            'configs', 'rviz', 'robot_description_rviz.rviz'),
             description='Full path to an RViz config file'
         )
 
