5be58a2d4dde707becd2d43194feda72b1505dbe
Pradheep <padhupradheep@gmail.com>
2025-11-18 06:25:04 +0100

    setting legacy to false

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -375,7 +375,7 @@ def generate_launch_description():
         )
 
     declare_use_legacy_cmd = DeclareLaunchArgument(
-            'use_legacy', default_value='True',
+            'use_legacy', default_value='False',
             description='Set legacy to True if you are using the old model'
         )
 
